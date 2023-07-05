118dc6908eb38f1d5c7eebd1d5f506a28683fb4d
Miras Mustafa <117440659+juzzmirha@users.noreply.github.com>
2023-07-05 21:46:20 +0600

    Add files via upload

---
+++ Notes/Databases/DTO/NoteCredentials.cs
@@ -0,0 +1,21 @@
+﻿using NotesDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace NotesDB.DTO
+{
+    public class NoteCredentials
+    {
+        public int id { get; set; }
+        public string text { get; set; }
+        public DateTime datetime { get; set; }
+        public int UserId { get; set; } // Внешний ключ
+
+    }
+}
---
+++ Notes/Databases/DTO/UserCredentials.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace NotesDB.DTO
+{
+    public class usercredentials
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+
+        public string login { get; set; }
+        public string password { get; set; }
+
+    }
+}
