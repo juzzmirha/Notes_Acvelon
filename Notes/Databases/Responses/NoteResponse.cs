eaf6563134ba268352373b4e944d7fd4d7ee9d83
Miras Mustafa <117440659+juzzmirha@users.noreply.github.com>
2023-07-05 21:48:46 +0600

    Add files via upload

---
+++ Notes/Databases/Responses/NoteResponse.cs
@@ -0,0 +1,18 @@
+﻿using NotesDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesDB.Responses
+{
+    public class NoteResponse
+    {
+        public int id { get; set; }
+        public string text { get; set; }
+        public DateTime datetime { get; set; }
+
+        public int UserId { get; set; } // Внешний ключ
+    }
+}
