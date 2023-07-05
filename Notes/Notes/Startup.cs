457528e19ee51d3fb972840d4d3e53ceceab1825
Miras Mustafa <117440659+juzzmirha@users.noreply.github.com>
2023-07-05 21:40:48 +0600

    Add files via upload

---
+++ Notes/Notes/Startup.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using NotesDB;
+using Raven.Abstractions.Data;
+using Microsoft.OpenApi.Models;
+using Notes.Services;
+
+namespace Notes
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Load connection string from appsettings.json
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<NotesDBContext>(options =>
+                options.UseNpgsql(connectionString));
+
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            });
+            services.AddScoped<INoteService, NoteService>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+            });
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
