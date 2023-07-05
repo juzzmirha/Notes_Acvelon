12008be210e58d888971564adbc73e679ab7a3b6
Miras Mustafa <117440659+juzzmirha@users.noreply.github.com>
2023-07-05 21:39:31 +0600

    Controllers

---
+++ Notes/Notes/Controllers/NotesController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NotesDB;
+using NotesDB.DTO;
+using NotesDB.Models;
+using System;
+
+namespace Notes.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NotesController : ControllerBase
+    {
+        private readonly NotesDBContext _dbContext;
+
+        public NotesController(NotesDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // Контроллер для создание заметок
+        [HttpPost("CreateNote")]
+        public IActionResult Create([FromBody] NoteCredentials note)
+        {
+            note.datetime = note.datetime.ToUniversalTime();
+
+            _dbContext.notecredentials.Add(note);
+            _dbContext.SaveChanges();
+
+            return Ok(new { message = "Note created successfully" });
+        }
+
+        // Контроллер для редактирование заметок
+        [HttpPut("UpdateNote/{id}")]
+        public IActionResult Update(int id, [FromBody] NoteCredentials noteDto)
+        {
+            var existingNote = _dbContext.notecredentials.Find(id);
+
+            if (existingNote == null)
+            {
+                return NotFound();
+            }
+
+            existingNote.text = noteDto.text;
+            _dbContext.SaveChanges();
+
+            return Ok(new { message = "Note updated successfully" });
+        }
+
+        // Контроллер для удаление заметок
+        [HttpDelete("DeleteNote/{id}")]
+        public IActionResult Delete(int id)
+        {
+            
+            var note = _dbContext.notecredentials.Find(id);
+
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.notecredentials.Remove(note);
+            _dbContext.SaveChanges();
+
+            return Ok(new { message = "Note deleted successfully" });
+        }
+
+        // Контроллер для просмотра заметок
+        [HttpGet("GetNoteById/{id}")]
+        public IActionResult GetNoteById(int id)
+        {
+            
+            NoteCredentials note = _dbContext.notecredentials.Find(id);
+
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(note);
+        }
+
+
+    }
+}
---
+++ Notes/Notes/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NotesDB;
+using NotesDB.DTO;
+using NotesDB.Models;
+using NotesDB.Responses;
+
+namespace Notes.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly NotesDBContext _dbContext;
+
+        public LoginController(NotesDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromBody] usercredentials usercredentials)
+        {
+            try
+            {
+                // Сохранение данных в базе данных
+                _dbContext.usercredentials.Add(usercredentials);
+                _dbContext.SaveChanges();
+
+                return Ok(new { message = "User created successfully" });
+            }
+            catch (DbUpdateException ex)
+            {
+                // Просмотр внутреннего исключения для получения подробностей об ошибке
+                Exception innerException = ex.InnerException;
+                string errorMessage = innerException?.Message;
+
+                
+
+               
+                return BadRequest(new { message = "Error occurred while saving user credentials", error = errorMessage });
+            }
+
+        }
+
+        
+
+    }
+}
