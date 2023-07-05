f1ffb38964334ea5531700cd052750a808dab7b7
Miras Mustafa <117440659+juzzmirha@users.noreply.github.com>
2023-07-05 21:47:18 +0600

    Add files via upload

---
+++ Notes/Databases/Models/Notes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NotesDB.DTO;
+namespace NotesDB.Models
+{
+    public class Notes
+    {
+        public int id { get; set; }
+        public string text { get; set; }
+        public DateTime datetime { get; set; }
+
+        public int UserId { get; set; } // Внешний ключ
+        public Users User { get; set; } // Навигационное свойство
+    }
+}
---
+++ Notes/Databases/Models/Users.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace NotesDB.Models
+{
+    public class Users
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+
+        public string login { get; set; }
+        public string password { get; set; }
+
+
+        public ICollection<Notes> Notes { get; set; } // Навигационное свойство
+
+    }
+}
