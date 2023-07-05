695887b0c566acb1f173b4beb841716dbcabbbe4
Miras Mustafa <117440659+juzzmirha@users.noreply.github.com>
2023-07-05 21:50:30 +0600

    Add files via upload

---
+++ Notes/Databases/NotesDBContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesDB.DTO;
+using NotesDB.Models;
+namespace NotesDB
+{
+    public class NotesDBContext : DbContext
+    {
+        public DbSet<usercredentials> usercredentials { get; set; }
+        public DbSet<NoteCredentials> notecredentials { get; set; }
+
+        public NotesDBContext(DbContextOptions<NotesDBContext> options) : base(options)
+        {
+
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Notes>()
+                .HasOne(n => n.User)
+                .WithMany(u => u.Notes)
+                .HasForeignKey(n => n.UserId);
+        }
+        
+
+    }
+}
